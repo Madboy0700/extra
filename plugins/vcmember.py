83200b6b4a766ada90d252fec9b764718a1647b5
meyit-4763 <sumeyyeomeryesilyurt@gmail.com>
2025-01-01 01:27:00 +0300

    Update vcmember.py

--- plugins/vcmember.py
+++ plugins/vcmember.py
@@ -1,42 +1,24 @@
-from pyrogram import Client, filters
-from pyrogram.types import ChatMember
+from pyrogram import filters
+from pyrogram.enums import ChatType
 from strings import get_string
 from YukkiMusic import app
 from YukkiMusic.utils import Yukkibin
 from YukkiMusic.utils.database import get_assistant, get_lang
 
 
-@app.on_message(filters.command(["vcuser", "vcusers", "vcmember", "vcmembers"]))
+@app.on_message(
+    filters.command(["vcuser", "vcusers", "vcmember", "vcmembers"]) & filters.admin
+)
 async def vc_members(client, message):
     try:
-        # Admin kontrolü yap
-        user_id = message.from_user.id
-        chat_id = message.chat.id
-        member = await client.get_chat_member(chat_id, user_id)
-
-        if member.status not in [ChatMember.ADMINISTRATOR, ChatMember.CREATOR]:
-            return await message.reply("Bu komutu kullanmak için admin olmalısınız.")
-        
-    except Exception as e:
-        return await message.reply(f"Bir hata oluştu: {str(e)}")
-
-    try:
-        # Dil ayarlarını al
         language = await get_lang(message.chat.id)
         _ = get_string(language)
-    except Exception:
-        # Dil ayarı alınamazsa varsayılan dil olarak İngilizceyi kullan
+    except BaseException:
         _ = get_string("en")
-
-    # Başlangıçta bir cevap mesajı gönder
     msg = await message.reply_text(_["V_C_1"])
-
-    # Yardımcı botu al
     userbot = await get_assistant(message.chat.id)
-
     TEXT = ""
     try:
-        # Sesli sohbetin üyelerini gez
         async for m in userbot.get_call_members(message.chat.id):
             chat_id = m.chat.id
             username = m.chat.username
@@ -47,18 +29,14 @@ async def vc_members(client, message):
             is_muted = bool(m.is_muted and not m.can_self_unmute)
             is_speaking = not m.is_muted
 
-            # Eğer kullanıcı bir grup içindeyse, grup başlığını al
             if m.chat.type != ChatType.PRIVATE:
                 title = m.chat.title
             else:
                 try:
-                    # Eğer kullanıcı özel mesajda ise, kullanıcıyı al
                     title = (await client.get_users(chat_id)).mention
-                except Exception:
-                    # Kullanıcı adı alınamazsa, adını al
+                except BaseException:
                     title = m.chat.first_name
 
-            # Üye bilgilerini formata yerleştir
             TEXT += _["V_C_2"].format(
                 title,
                 chat_id,
@@ -71,8 +49,6 @@ async def vc_members(client, message):
                 is_left,
             )
             TEXT += "\n\n"
-
-        # Eğer metin çok uzunsa, Yukkibin ile kısalt ve linki gönder
         if len(TEXT) < 4000:
             await msg.edit(TEXT or _["V_C_3"])
         else:
@@ -81,7 +57,5 @@ async def vc_members(client, message):
                 _["V_C_4"].format(link),
                 disable_web_page_preview=True,
             )
-
     except ValueError as e:
-        # Eğer bir hata oluşursa, hata mesajı gönder
         await msg.edit(_["V_C_5"])
