ab8f7e246a506297a1e6bd4116038283815695c8
meyit-4763 <sumeyyeomeryesilyurt@gmail.com>
2025-01-01 02:18:03 +0300

    Update vcmember.py

--- plugins/vcmember.py
+++ plugins/vcmember.py
@@ -5,28 +5,20 @@ from YukkiMusic import app
 from YukkiMusic.utils import Yukkibin
 from YukkiMusic.utils.database import get_assistant, get_lang
 
+
 @app.on_message(
     filters.command(["vcuser", "vcusers", "vcmember", "vcmembers"]) & filters.admin
 )
 async def vc_members(client, message):
-    # Dil ayarlarını almak
     try:
         language = await get_lang(message.chat.id)
-        _ = get_string(language)  # Dil metinlerini al
+        _ = get_string(language)
     except BaseException:
-        _ = get_string("en")  # Hata durumunda varsayılan olarak İngilizce kullan
-
-    # Başlangıçta mesaj gönder
+        _ = get_string("en")
     msg = await message.reply_text(_["V_C_1"])
-    
-    # Chat için userbot'ı al
     userbot = await get_assistant(message.chat.id)
-    
-    # Katılımcı bilgilerini tutacak değişken
     TEXT = ""
-    
     try:
-        # Görüşme üyelerini al
         async for m in userbot.get_call_members(message.chat.id):
             chat_id = m.chat.id
             username = m.chat.username
@@ -37,20 +29,18 @@ async def vc_members(client, message):
             is_muted = bool(m.is_muted and not m.can_self_unmute)
             is_speaking = not m.is_muted
 
-            # Katılımcı adı ve bilgileri için formatlama
             if m.chat.type != ChatType.PRIVATE:
-                title = m.chat.title  # Grup adı
+                title = m.chat.title
             else:
                 try:
-                    title = (await client.get_users(chat_id)).mention  # Kullanıcı adı
+                    title = (await client.get_users(chat_id)).mention
                 except BaseException:
-                    title = m.chat.first_name  # Kullanıcının ismi
+                    title = m.chat.first_name
 
-            # Katılımcı bilgilerini metne ekle
             TEXT += _["V_C_2"].format(
                 title,
                 chat_id,
-                username or "N/A",  # Kullanıcı adı yoksa 'N/A' yazdır
+                username,
                 is_video_enabled,
                 is_screen_sharing_enabled,
                 is_hand_raised,
@@ -59,17 +49,13 @@ async def vc_members(client, message):
                 is_left,
             )
             TEXT += "\n\n"
-        
-        # Mesaj uzunluğu 4000 karakteri geçiyorsa, Yukkibin ile link oluştur
         if len(TEXT) < 4000:
-            await msg.edit(TEXT or _["V_C_3"])  # Eğer metin boşsa, alternatif mesaj
+            await msg.edit(TEXT or _["V_C_3"])
         else:
-            link = await Yukkibin(TEXT)  # Uzun metin için Yukkibin linki oluştur
+            link = await Yukkibin(TEXT)
             await msg.edit(
-                _["V_C_4"].format(link),  # Linki mesajda göster
+                _["V_C_4"].format(link),
                 disable_web_page_preview=True,
             )
-    
     except ValueError as e:
-        # Bir hata oluşursa, hata mesajı gönder
         await msg.edit(_["V_C_5"])
