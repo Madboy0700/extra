865a946b1776565583d852991c9afe16c567a0ae
Madboy0700 <bobimobi07@gmail.com>
2025-01-01 19:12:51 +0300

    Update chatgpt.py

--- plugins/chatgpt.py
+++ plugins/chatgpt.py
@@ -9,22 +9,23 @@ from YukkiMusic import app
 async def chatgpt_chat(bot, message):
     if len(message.command) < 2 and not message.reply_to_message:
         await message.reply_text(
-            "Example:\n\n`/ai write simple website code using html css, js?`"
+            "Örnek:\n\n`/ai basit bir web sitesi kodu yaz html css, js kullanarak?`"
         )
         return
 
     if message.reply_to_message and message.reply_to_message.text:
-        user_input = message.reply_to_message.text
+        user_input = message.reply_to_message.text  # Yanıtlanan mesajın metnini al
     else:
-        user_input = " ".join(message.command[1:])
+        user_input = " ".join(message.command[1:])  # Komutun geri kalanını birleştir
 
-    await bot.send_chat_action(message.chat.id, ChatAction.TYPING)
-    results = await api.chatgpt(user_input)
-    await message.reply_text(results)
+    await bot.send_chat_action(message.chat.id, ChatAction.TYPING)  # Yazma eylemi gönder
+    results = await api.chatgpt(user_input)  # API'den yanıt al
+    await message.reply_text(results)  # Yanıtı gönder
 
 
-__MODULE__ = "CʜᴀᴛGᴘᴛ"
+__MODULE__ = "CʜᴀᴛGᴘᴛ"  # Modül adı
 __HELP__ = """
-/advice - ɢᴇᴛ ʀᴀɴᴅᴏᴍ ᴀᴅᴠɪᴄᴇ ʙʏ ʙᴏᴛ
-/ai [ǫᴜᴇʀʏ] - ᴀsᴋ ʏᴏᴜʀ ǫᴜᴇsᴛɪᴏɴ ᴡɪᴛʜ ᴄʜᴀᴛɢᴘᴛ's ᴀɪ
-/gemini [ǫᴜᴇʀʏ] - ᴀsᴋ ʏᴏᴜʀ ǫᴜᴇsᴛɪᴏɴ ᴡɪᴛʜ ɢᴏᴏɢʟᴇ's ɢᴇᴍɪɴɪ ᴀɪ"""
+/advice - Bot tarafından rastgele tavsiye al
+/ai [soru] - ChatGPT'nin AI'si ile sorunu sor
+/gemini [soru] - Google'ın Gemini AI'si ile sorunu sor
+"""
