8de824ea7c8cc0f0dc4a54315c88819d68994ab4
Madboy0700 <bobimobi07@gmail.com>
2025-01-01 19:19:53 +0300

    Update gemini.py

--- plugins/gemini.py
+++ plugins/gemini.py
@@ -7,28 +7,28 @@ from YukkiMusic import app
 
 @app.on_message(filters.command(["gemini"]))
 async def gemini_handler(client, message):
-    await app.send_chat_action(message.chat.id, ChatAction.TYPING)
+    await app.send_chat_action(message.chat.id, ChatAction.TYPING)  # Kullanıcıya yazma eylemi göster
     if (
         message.text.startswith(f"/gemini@{app.username}")
         and len(message.text.split(" ", 1)) > 1
     ):
-        user_input = message.text.split(" ", 1)[1]
+        user_input = message.text.split(" ", 1)[1]  # Kullanıcının girdiği metni al
     elif message.reply_to_message and message.reply_to_message.text:
-        user_input = message.reply_to_message.text
+        user_input = message.reply_to_message.text  # Yanıtlanan mesajın metnini al
     else:
         if len(message.command) > 1:
-            user_input = " ".join(message.command[1:])
+            user_input = " ".join(message.command[1:])  # Komutun geri kalanını al
         else:
-            await message.reply_text("ᴇxᴀᴍᴘʟᴇ :- `/gemini who is lord ram`")
+            await message.reply_text("Örnek :- `/gemini kimdir lord ram`")  # Kullanıcıya örnek ver
             return
 
     try:
-        response = api.gemini(user_input)
-        await app.send_chat_action(message.chat.id, ChatAction.TYPING)
-        x = response["results"]
+        response = api.gemini(user_input)  # API'den yanıt al
+        await app.send_chat_action(message.chat.id, ChatAction.TYPING)  # Yazma eylemi göster
+        x = response["results"]  # API yanıtındaki sonuçları al
         if x:
-            await message.reply_text(x, quote=True)
+            await message.reply_text(x, quote=True)  # Sonuçları yanıt olarak gönder
         else:
-            await message.reply_text("sᴏʀʀʏ sɪʀ! ᴘʟᴇᴀsᴇ Tʀʏ ᴀɢᴀɪɴ")
+            await message.reply_text("Üzgünüm! Lütfen tekrar deneyin.")  # Sonuç yoksa hata mesajı gönder
     except requests.exceptions.RequestException as e:
-        pass
+        pass  # Hata durumunda hiçbir şey yapma
