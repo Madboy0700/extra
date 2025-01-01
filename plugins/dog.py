991f13fea2ad3bd0df276d0e0dd44ed8cd4546be
Madboy0700 <bobimobi07@gmail.com>
2025-01-01 19:13:46 +0300

    Update dog.py

--- plugins/dog.py
+++ plugins/dog.py
@@ -11,40 +11,41 @@ from pyrogram.types import (
 from YukkiMusic import app
 
 
+# Klavye düzeni
 close_keyboard = InlineKeyboardMarkup(
     [
-        [InlineKeyboardButton(text="Rᴇғʀᴇsʜ", callback_data="refresh_dog")],
-        [InlineKeyboardButton(text="〆 ᴄʟᴏsᴇ 〆", callback_data="close")],
+        [InlineKeyboardButton(text="Yenile", callback_data="refresh_dog")],
+        [InlineKeyboardButton(text="〆 Kapat 〆", callback_data="close")],
     ]
 )
 
 
 @app.on_message(filters.command(["dogs", "dog"]) & ~BANNED_USERS)
 async def dog(c, m: Message):
-    r = requests.get("https://random.dog/woof.json")
-    if r.status_code == 200:
-        data = r.json()
-        dog_url = data["url"]
-        if dog_url.endswith(".gif"):
-            await m.reply_animation(dog_url, reply_markup=close_keyboard)
+    r = requests.get("https://random.dog/woof.json")  # Rastgele köpek resmi almak için API'ye istek gönder
+    if r.status_code == 200:  # İstek başarılıysa
+        data = r.json()  # JSON verisini al
+        dog_url = data["url"]  # Köpek resminin URL'sini al
+        if dog_url.endswith(".gif"):  # Eğer resim bir GIF ise
+            await m.reply_animation(dog_url, reply_markup=close_keyboard)  # GIF'i gönder
         else:
-            await m.reply_photo(dog_url, reply_markup=close_keyboard)
+            await m.reply_photo(dog_url, reply_markup=close_keyboard)  # Fotoğrafı gönder
     else:
-        await m.reply_text("Failed to fetch dog picture 🐕")
+        await m.reply_text("Köpek resmi alınamadı 🐕")  # İstek başarısızsa mesaj gönder
 
 
 @app.on_callback_query(filters.regex("refresh_dog") & ~BANNED_USERS)
 async def refresh_dog(c, m: CallbackQuery):
-    r = requests.get("https://random.dog/woof.json")
-    if r.status_code == 200:
-        data = r.json()
-        dog_url = data["url"]
-        if dog_url.endswith(".gif"):
-            await m.edit_message_animation(dog_url, reply_markup=close_keyboard)
+    r = requests.get("https://random.dog/woof.json")  # Yeniden köpek resmi almak için API'ye istek gönder
+    if r.status_code == 200:  # İstek başarılıysa
+        data = r.json()  # JSON verisini al
+        dog_url = data["url"]  # Köpek resminin URL'sini al
+        if dog_url.endswith(".gif"):  # Eğer resim bir GIF ise
+            await m.edit_message_animation(dog_url, reply_markup=close_keyboard)  # GIF'i güncelle
         else:
             await m.edit_message_media(
-                InputMediaPhoto(media=dog_url),
+                InputMediaPhoto(media=dog_url),  # Fotoğrafı güncelle
                 reply_markup=close_keyboard,
             )
     else:
-        await m.edit_message_text("Failed to refresh dog picture 🐕")
+        await m.edit_message_text("Köpek resmi yenilenemedi 🐕")  # İstek başarısızsa mesaj gönder
